82c11feebc87f1d84c730100cd3cd1c3e6dff34a
Vinicius Gabriel <viniciusgsa99@gmail.com>
2022-04-12 19:12:36 -0300

    Sétimo requisito concluídogit statusgit statusgit status #VQV finalmente hahaha

--- src/challenges.js
+++ src/challenges.js
@@ -57,9 +57,27 @@ function highestCount(maiorValor) {
   return contador;
 }
 
+function negativePositive(num) {
+  let positivo;
+  if (num < 0) {
+    return num *= -1;
+  }
+  return num;
+}
+
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let positionCat1 = mouse - cat1;
+  let positionCat2 = mouse - cat2;
+  positionCat1 = negativePositive(positionCat1);
+  positionCat2 = negativePositive(positionCat2);
+  if (positionCat1 > positionCat2) {
+    return 'cat2';
+  } if (positionCat2 > positionCat1) {
+    return 'cat1';
+  } if (positionCat1 === positionCat2) {
+    return 'os gatos trombam e o rato foge';
+  }
 }
 
 // Desafio 8
