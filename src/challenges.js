263c32af638f1c4eb5d505196301151055bac53c
Vinicius Gabriel <viniciusgsa99@gmail.com>
2022-04-12 16:16:22 -0300

    Quinto requisito concluído.

--- src/challenges.js
+++ src/challenges.js
@@ -23,9 +23,13 @@ function concatName(array) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let vitorias = wins * 3;
+  let empates = ties * 1;
+  return vitorias + empates;
 }
+
+console.log(footballPoints(0, 0));
 
 // Desafio 6
 function highestCount() {
