583a7db6d0549bbf5139eb80de46d4d55667e054
Vinicius Gabriel <viniciusgsa99@gmail.com>
2022-04-20 13:20:58 -0300

    Décimo segundo e Décimo terceiro requisitos concluídos

--- src/challenges2.js
+++ src/challenges2.js
@@ -37,14 +37,58 @@ function generatePhoneNumber(nums) {
   return `(${nums[0]}${nums[1]}) ${nums[2]}${nums[3]}${nums[4]}${nums[5]}${nums[6]}-${nums[7]}${nums[8]}${nums[9]}${nums[10]}`;
 }
 
+// Retorna True caso cada lado seja menor que a soma dos outros dois lados
+function sumSides(sideA, sideB, sideC) {
+  const line1 = sideB + sideC;
+  const line2 = sideA + sideC;
+  const line3 = sideA + sideB;
+  if (sideA < line1 && sideB < line2 && sideC < line3) {
+    return true;
+  }
+}
+
+// Retornar True caso cada valor seja maior que o valor absoluto da diferença entre os dois dois valores
+function absoluteValue(valueA, valueB, valueC) {
+  const value = Math.abs(valueB - valueC); // Explicação da função Math.abs() no site https://www.w3schools.com/jsref/jsref_abs.asp
+  const value2 = Math.abs(valueA - valueC); // https://www.w3schools.com/jsref/jsref_abs.asp
+  const value3 = Math.abs(valueA - valueB); // https://www.w3schools.com/jsref/jsref_abs.asp
+  if (valueA > value && valueB > value2 && valueC > value3) {
+    return true;
+  }
+}
+
 // Desafio 12
-function triangleCheck() {
+function triangleCheck(lineA, lineB, lineC) {
   // seu código aqui
+  let testSides = sumSides(lineA, lineB, lineC);
+  let testValue = absoluteValue(lineA, lineB, lineC);
+  if (testSides === true && testValue === true) {
+    return true;
+  }
+  return false;
+}
+
+// Convert e soma os números de uma string
+function convertToNumber(string) {
+  let arrayNum = string.match(/[0-9]/g); // Como retirar apenas os números de uma string referência https://www.w3schools.com/jsref/jsref_regexp_0-9.asp
+  let counter = 0;
+  for (let index = 0; index < arrayNum.length; index += 1) {
+    let convert = Number(arrayNum[index]); // Como converter uma string em número referência https://www.w3schools.com/jsref/jsref_number.asp
+    counter += convert;
+  }
+  return counter;
 }
 
 // Desafio 13
-function hydrate() {
+function hydrate(string) {
   // seu código aqui
+  const sum = convertToNumber(string);
+  if (sum === 1) {
+    return '1 copo de água';
+  }
+  if (sum > 1) {
+    return `${sum} copos de água`;
+  }
 }
 
 module.exports = {
